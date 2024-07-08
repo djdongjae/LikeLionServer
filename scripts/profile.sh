7d3c67b2e5b15f440ab0825e2178ac2b93a58f45
djdongjae <jk47wee@gmail.com>
2024-07-08 16:34:01 +0900

    refactor: localhost to 127.0.0.1

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 function find_idle_profile() {
-    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
+    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" https://cookie-house.store/profile)
 
     if [ ${RESPONSE_CODE} -ge 400 ]
     then
       CURRENT_PROFILE=real2
     else
-      CURRENT_PROFILE=$(curl -s http://localhost/profile)
+      CURRENT_PROFILE=$(curl -s https://cookie-house.store/profile)
     fi
 
     if [ ${CURRENT_PROFILE} == real1 ]
